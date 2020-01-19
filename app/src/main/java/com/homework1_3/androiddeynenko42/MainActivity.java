904778b9e453c47fc8aec5a4d5f323fe61bd80a8
Alexandr Deynenko <alex.deko.69@gmail.com>
2020-01-19 18:47:51 +0300

    cleanError1

--- app/src/main/java/com/homework1_3/androiddeynenko42/MainActivity.java
+++ app/src/main/java/com/homework1_3/androiddeynenko42/MainActivity.java
@@ -13,7 +13,6 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -25,11 +24,12 @@ public class MainActivity extends AppCompatActivity {
     private final static String TITLE = "title";
     private final static String SUBTITLE = "subtitle";
     private final static String TEXT = "text";
+    private final static String PREF = "pref";
+    List<Map<String, String>> simpleAdapterContent;
     private SharedPreferences sharedPref;
     private ListView list;
     private String result;
-    private String[] content ;
-    private SharedPreferences.Editor myEditor;
+    private String[] content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,46 +46,56 @@ public class MainActivity extends AppCompatActivity {
 
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
-                final View item = (TextView) parent.getItemAtPosition(position);
-                view.animate().setDuration(2000).alpha(0)
+            public void onItemClick(AdapterView<?> parent, final View view, final int position, long id) {
+                //final View item = (TextView) parent.getItemAtPosition(position);
+                view.animate().setDuration(20).alpha(0)
                         .withEndAction(new Runnable() {
                             @Override
                             public void run() {
-                                list.removeView(item);
+                                // list.removeView(item);
+                                simpleAdapterContent.remove(position);
                                 listContentAdapter.notifyDataSetChanged();
                                 view.setAlpha(1);
                             }
                         });
-                listContentAdapter.notifyDataSetChanged();
+                //  list.removeView(item);
+
+                // listContentAdapter.notifyDataSetChanged();
             }
         });
 
         final SwipeRefreshLayout swipeLayout = findViewById(R.id.swiperefresh);
         swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             // Будет вызван, когда пользователь потянет список вниз
-            @Override public void onRefresh() {
+            @Override
+            public void onRefresh() {
                 updateList();
                 swipeLayout.setRefreshing(false);
             }
         });
-        }
+    }
 
-        private void updateList(){
-            sharedPref = getSharedPreferences(getString(R.string.large_text), Context.MODE_PRIVATE);
-            myEditor = sharedPref.edit();
-        if (!getString(R.string.large_text).contentEquals(sharedPref.toString())) {
-            myEditor.putString(TEXT, getString(R.string.large_text));
-            myEditor.apply();
-            result = sharedPref.getString(TEXT, getString(R.string.large_text));
+    private void updateList() {
+        sharedPref = getSharedPreferences(PREF, Context.MODE_PRIVATE);
+        // SharedPreferences.Editor myEditor = sharedPref.edit();
+        if (sharedPref.contains(TEXT)) {
+            result = sharedPref.getString(TEXT, null);
+
+        } else {
+            String largeText = getString(R.string.large_text);
+
+            sharedPref.edit()
+                    .putString(TEXT, largeText)
+                    .apply();
+            result = largeText;
         }
-            content = prepareContent();
-        }
+        content = prepareContent();
+    }
 
     @NonNull
     private BaseAdapter createAdapter(String[] values) {
         list = findViewById(R.id.list);
-        List<Map<String, String>> simpleAdapterContent = new ArrayList<>();
+        simpleAdapterContent = new ArrayList<>();
 
         for (String value : values) {
             Map<String, String> row = new HashMap<>();
